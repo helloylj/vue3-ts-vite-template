9c8ff52466a4f31d5c731ef2aa3d8980fd3d741c
yulj <yulj@ipampas.com>
2022-01-21 18:20:16 +0800

    build: 修改配置文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/ban-types': 'off'
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off'
   }
 }
