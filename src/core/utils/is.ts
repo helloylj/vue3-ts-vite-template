1c0c26c332444d2278c69ef1302334eecc108eb5
yulj <yulj@ipampas.com>
2022-01-21 18:17:04 +0800

    feat(utils): 新增utils工具函数

--- src/core/utils/is.ts
+++ src/core/utils/is.ts
@@ -94,3 +94,10 @@ export function isMap(val: unknown): val is Map<any, any> {
 export const isServer = typeof window === 'undefined'
 
 export const isClient = !isServer
+
+export function isUrl(path: string): boolean {
+  const reg =
+    // eslint-disable-next-line no-useless-escape
+    /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(?::\d+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/
+  return reg.test(path)
+}
